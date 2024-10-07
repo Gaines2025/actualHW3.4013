172487ab15cf363218ab3af7d364cf02b6798ac0
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-07 14:29:44 -0500

    Update leagues.php

--- leagues.php
+++ leagues.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
 require_once("model-leagues.php");
+
 $pageTitle = "Soccer Leagues";
 include "view-header.php";
 $leagues = selectLeagues();
-
 include "view-leagues.php";
 include "view-footer.php";
 ?>
