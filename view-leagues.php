47858f30130d9c894729be2b30192d2a1dc08ad5
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-09 13:47:56 -0500

    Create view-leagues.php

---
+++ view-leagues.php
@@ -0,0 +1,21 @@
+<h1>Leagues</h1>
+<table class="table table-striped">
+  <thead>
+    <th>League</th>
+    <th>Country of League</th>
+    <th>League ID#</th>
+  </thead>
+  <tbody>
+<?php
+  while($league = $leagues->fetch_assoc()) {
+?>
+    <tr>
+      <td><?php echo $league['league_name']; ?></td>
+      <td><?php echo $league['country_origin']; ?></td>
+      <td><?php echo $league['league_ID']; ?></td>
+    </tr>
+  }
+    
+?>
+  </tbody>
+</table>
