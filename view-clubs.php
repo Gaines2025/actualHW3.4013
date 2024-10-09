234a5b79695bd20fd24e888bb0e8b811e8f92ca0
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-09 14:17:03 -0500

    Create view-clubs.php

---
+++ view-clubs.php
@@ -0,0 +1,23 @@
+<h1>Futbol Clubs</h1>
+<table class="table table-striped">
+  <thead>
+    <th>Club Team Name</th>
+    <th>Country of Club</th>
+    <th>City of Club</th>
+    <th>Club ID</th>
+  </thead>
+  <tbody>
+<?php
+  while($club = $clubs->fetch_assoc()) {
+?>
+    <tr>
+      <td><?php echo $club['club_name']; ?></td>
+      <td><?php echo $club['club_country']; ?></td>
+      <td><?php echo $club['club_city']; ?></td>
+      <td><?php echo $club['club_ID']; ?></td>
+    </tr>
+<?php
+    }
+?>
+  </tbody>
+</table>
