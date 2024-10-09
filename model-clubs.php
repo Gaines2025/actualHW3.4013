a520d4244f89c144b288df496143904e158d2f3d
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-09 14:13:09 -0500

    Update model-clubs.php

--- model-clubs.php
+++ model-clubs.php
@@ -1,8 +1,8 @@
 <?php
-function selectLeagues() {
+function selectClubs() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT league_ID, league_name, country_origin FROM league");
+        $stmt = $conn->prepare("SELECT club_ID, club_name, club_city, club_country FROM Club");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
