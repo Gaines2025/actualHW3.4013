76cd6d117aeaca4425bc53aec6fd5406fdf1c72a
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-14 13:34:35 -0500

    Update view-players-on-teams.php

--- view-players-on-teams.php
+++ view-players-on-teams.php
@@ -1,1 +1,22 @@
-
+<h1>Players on Teams</h1>
+<table class="table table-striped">
+  <thead>
+    <th>League</th>
+    <th>Country of League</th>
+    <th>League ID#</th>
+  </thead>
+  <tbody>
+<?php
+  while($league = $leagues->fetch_assoc()) {
+?>
+    <tr>
+      <td><?php echo $league['league_name']; ?></td>
+      <td><?php echo $league['country_origin']; ?></td>
+      <td><?php echo $league['league_ID']; ?></td>
+      <td><a href="players-playing.php?id=<?php echo $league['league_name']; ?>"> </a></td>
+    </tr>
+<?php
+    }
+?>
+  </tbody>
+</table>
