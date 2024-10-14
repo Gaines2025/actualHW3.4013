a5ac19ef3f92d92c343d84add32eb61c56de5d5e
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-14 18:12:28 -0500

    Update model-players-on-clubs.php

--- model-players-on-clubs.php
+++ model-players-on-clubs.php
@@ -2,7 +2,7 @@
 function selectPlayersOnClubs($lid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.club_ID, club_name, club_city, club_country FROM Club c join Player p on c.club_ID = p.club_ID where p.league_ID = 1");
+        $stmt = $conn->prepare("SELECT c.club_ID, club_name, club_city, club_country FROM Club c join Player p on c.club_ID = p.club_ID where p.club_ID = ?");
         $stmt->bind_param("i",$lid);
         $stmt->execute();
         $result = $stmt->get_result();
