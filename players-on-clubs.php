00fdbc30d2abf8305c3ddc1c9389ed1ecb1e34a2
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-16 13:49:02 -0500

    Update players-on-clubs.php

--- players-on-clubs.php
+++ players-on-clubs.php
@@ -4,7 +4,7 @@ require_once("model-players-on-clubs.php");
 
 $pageTitle = "Players on Clubs";
 include "view-header.php";
-$playersClubs = selectPlayersOnClubs($_GET['id']);
+$playerClubs = selectPlayersOnClubs($_GET['id']);
 include "view-players-on-clubs.php";
 include "view-footer.php";
 ?>
