3e5ba4a732d5530a4634c05399d53d93a18df16f
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-21 12:44:01 -0500

    Create clubs-in-league.php

---
+++ clubs-in-league.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-clubs-in-league.php");
+
+$pageTitle = "Clubs in League";
+include "view-header.php";
+$clubsInLeagues = selectClubsInLeague($_POST['cid']);
+include "view-clubs-in-leagues.php";
+include "view-footer.php";
+?>
