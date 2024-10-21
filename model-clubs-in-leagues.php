fe12f4fc4c473a4673b5d132f974b757e5eeb155
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-21 12:44:24 -0500

    Create model-clubs-in-leagues.php

---
+++ model-clubs-in-leagues.php
@@ -0,0 +1,16 @@
+<?php
+function selectPlayersOnClubs($lid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player_name, player_country, player_age, player_league, player_ID From Player p Where p.club_ID = ?");
+        $stmt->bind_param("i",$lid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
