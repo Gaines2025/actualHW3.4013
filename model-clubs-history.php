89ab649d24e0b6741b6d69afe615d76763ff9f02
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-21 13:42:00 -0500

    Update model-clubs-history.php

--- model-clubs-history.php
+++ model-clubs-history.php
@@ -12,4 +12,19 @@ function selectClubs() {
         throw $e;
     }
 }
+
+function selectPlayersOnClubs($lid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT player_name, player_country, player_age, player_league, player_ID From Player p Where p.club_ID = ?");
+        $stmt->bind_param("i",$lid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
