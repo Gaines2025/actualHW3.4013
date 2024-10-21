80fc0427f501655174faede7daca1fce926cbb47
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-21 13:21:15 -0500

    Update clubs-in-league.php

--- clubs-in-league.php
+++ clubs-in-league.php
@@ -5,6 +5,6 @@ require_once("model-clubs-in-league.php");
 $pageTitle = "Clubs in League";
 include "view-header.php";
 $clubsInLeagues = selectClubsInLeague($_POST['cid']);
-include "view-clubs-in-leagues.php";
+include "view-clubs-in-league.php";
 include "view-footer.php";
 ?>
