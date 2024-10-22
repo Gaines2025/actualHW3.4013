d73045706f840dbe790d393dcb93b2ed966d341f
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-22 09:29:50 -0500

    Update view-clubs-history.php

--- view-clubs-history.php
+++ view-clubs-history.php
@@ -7,8 +7,8 @@
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        <?php echo $league['league_name']; ?>
-        <?php echo $league['league_ID']; ?>
+        <p>League #</p><?php echo $league['league_ID']; ?>
+        <p>  </p><?php echo $league['league_name']; ?>
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
