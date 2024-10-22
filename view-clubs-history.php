11b432b231675e7b91c6cac08ca07d4924f63f66
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-22 09:25:43 -0500

    Update view-clubs-history.php

--- view-clubs-history.php
+++ view-clubs-history.php
@@ -1,12 +1,14 @@
 <h1>Futbol Clubs</h1>
 <div class="accordion" id="accordionExample">
 <?php
-  while($league = $leaguess->fetch_assoc()) {
+  while($league = $leagues->fetch_assoc()) 
+  {
 ?>
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        <?php echo $club['club_name']; ?>
+        <?php echo $league['league_name']; ?>
+        <?php echo $league['league_ID']; ?>
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
@@ -14,13 +16,6 @@
         <strong>This is the first item's accordion body.</strong>
       </div>
     </div>
-    <tr>
-      <td><?php echo $club['club_name']; ?></td>
-      <td><?php echo $club['club_country']; ?></td>
-      <td><?php echo $club['club_city']; ?></td>
-      <td><?php echo $club['club_ID']; ?></td>
-      <td><a href="players-on-clubs.php?id=<?php echo $club['club_ID']; ?>">Clubs</a></td>
-    </tr>
 <?php
     }
 ?>
