883409b5cb7f29a9240dde1edde5cd4bb4992a5d
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-23 11:53:14 -0500

    Update view-clubs-history.php

--- view-clubs-history.php
+++ view-clubs-history.php
@@ -7,14 +7,14 @@
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        <?php echo $league['league_ID'] . ") " . $league['country_origin']; ?>
+        <?php echo $league['league_ID'] . ") " . $league['league_name']; ?>
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div class="accordion-body">
         <strong>
           <ul class="list-group">
-            <li class="list-group-item"><?php echo "The league the country is from is: " . $league['league_name']; ?></li>
+            <li class="list-group-item"><?php echo "The league the country is from is: " . $league['country_origin']; ?></li>
           </ul>
         </strong>
     <?php
