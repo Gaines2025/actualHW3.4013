98bc4542978e0d6d06d302a0c34d3b6d20f792ab
Gaines2025 <maalakai.gaines@ou.edu>
2024-11-11 14:11:14 -0600

    Update leagues.php

--- leagues.php
+++ leagues.php
@@ -4,6 +4,36 @@ require_once("model-leagues.php");
 
 $pageTitle = "Soccer Leagues";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+      case "Add":
+        if (insertClubs($_POST['cName'], $_POST['cCity'], $_POST['cCountry'])) {
+            echo '<div class="alert alert-success" role="alert">Club has succesfully been added!</div>';
+            }
+        else {
+            echo '<div class="alert alert-danger" role="alert">Error Adding Club.</div>';
+        }
+        break;
+      case "Delete":
+        if (deleteClubs($_POST['cid'])) {
+            echo '<div class="alert alert-success" role="alert">Club Deleted!</div>';
+            }
+        else {
+            echo '<div class="alert alert-danger" role="alert">Error Deleting Club.</div>';
+        }
+        break;
+      case "Edit":
+        if (updateClubs($_POST['cName'], $_POST['cCity'], $_POST['cCountry'], $_POST['cid'])) {
+            echo '<div class="alert alert-success" role="alert">Club has been edited!</div>';
+            }
+        else {
+            echo '<div class="alert alert-danger" role="alert">Error editing Club.</div>';
+        }
+        break;
+  }
+}
+
 $leagues = selectLeagues();
 include "view-leagues.php";
 include "view-footer.php";
