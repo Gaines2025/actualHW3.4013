0d5bdc998fb1a8668ccb37f603e1e2da9dcc6cc5
Gaines2025 <maalakai.gaines@ou.edu>
2024-11-13 17:00:51 -0600

    Update managers.php

--- managers.php
+++ managers.php
@@ -4,6 +4,36 @@ require_once("model-managers.php");
 
 $pageTitle = "Soccer Managers";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+      case "Add":
+        if (insertManagers($_POST['mName'], $_POST['mClub'], $_POST['mCountry'], $_POST['mAge'])) {
+            echo '<div class="alert alert-success" role="alert">Manager has succesfully been added!</div>';
+            }
+        else {
+            echo '<div class="alert alert-danger" role="alert">Error Adding Manager.</div>';
+        }
+        break;
+      case "Delete":
+        if (deleteClubs($_POST['mid'])) {
+            echo '<div class="alert alert-success" role="alert">Manager Deleted!</div>';
+            }
+        else {
+            echo '<div class="alert alert-danger" role="alert">Error Deleting Manager.</div>';
+        }
+        break;
+      case "Edit":
+        if (updateClubs($_POST['mName'], $_POST['mClub'], $_POST['mCountry'], $_POST['mAge'], $_POST['mid'])) {
+            echo '<div class="alert alert-success" role="alert">Manager has been edited!</div>';
+            }
+        else {
+            echo '<div class="alert alert-danger" role="alert">Error editing Manager.</div>';
+        }
+        break;
+  }
+}
+
 $managers = selectManagers();
 include "view-managers.php";
 include "view-footer.php";
