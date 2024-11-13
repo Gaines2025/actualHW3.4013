09474c9618ee08def9e8a8d5f45754e8110249de
Gaines2025 <maalakai.gaines@ou.edu>
2024-11-13 14:22:47 -0600

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -1,4 +1,7 @@
 <h1>Futbol Players</h1>
+<?php
+include view-players-addPlayer.php
+?>
 <table class="table table-striped">
   <thead>
     <th>Player's Name</th>
@@ -15,6 +18,22 @@
       <td><?php echo $player['player_country']; ?></td>
       <td><?php echo $player['player_league']; ?></td>
       <td><?php echo $player['player_age']; ?></td>
+      <td>
+        <?php
+          include "view-clubs-editClub.php";
+        ?>
+      
+      </td>
+      <td>
+        <form method="post" action="">
+          <input type="hidden" name="pid" value="<?php echo $player['player_ID']; ?>">
+          <input type="hidden" name="actionType" value="Delete">
+          <button type="submit" class="btn btn-outline-primary" onclick="return confirm('Are you sure?');">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ban" viewBox="0 0 16 16">
+              <path d="M15 8a6.97 6.97 0 0 0-1.71-4.584l-9.874 9.875A7 7 0 0 0 15 8M2.71 12.584l9.874-9.875a7 7 0 0 0-9.874 9.874ZM16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0"/>
+            </svg></button>
+        </form>
+      </td>
     </tr>
 <?php
     }
