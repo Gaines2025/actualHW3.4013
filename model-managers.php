d9f394f8b1dfeded89b611d3797b7eac74b600a3
Gaines2025 <maalakai.gaines@ou.edu>
2024-11-13 16:56:50 -0600

    Update model-managers.php

--- model-managers.php
+++ model-managers.php
@@ -12,4 +12,47 @@ function selectManagers() {
         throw $e;
     }
 }
+
+function updateManagers($mName, $mCountry, $mClub, , $mAge, $mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Manager` SET manager_name = ?, manager_age = ?, manager_club = ?, manager_country = ? where manager_ID = ?");
+        $stmt->bind_param("sssi", $mName, $mCountry, $mClub, $mAge);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertClubs($mName, $mCountry, $mClub, , $mAge) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Manager` ('manager_ID', 'manager_name', 'manager_age', 'manager_club', 'manager_country') VALUES (NULL, ?, ?, ?, ?)");
+        $stmt->bind_param("sssi", $cName, $cCity, $cCountry, $mAge);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteClubs($mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from Club where manager_ID=?");
+        $stmt->bind_param("i", $mid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
