277654dd64bdd8a6f60db6c458f184ad3f16fe41
Gaines2025 <maalakai.gaines@ou.edu>
2024-11-13 15:07:47 -0600

    Update clubs.php

--- clubs.php
+++ clubs.php
@@ -5,14 +5,6 @@ require_once("model-clubs.php");
 $pageTitle = "Soccer Clubs";
 include "view-header.php";
 
-if (isset($_POST['actionType'])) {
-    switch ($_POST['actionType']) {
-      case "Add":
-        insertClubs($_POST['cName'], $_POST['cCity'], $_POST['cCountry']);
-        break;
-  }
-}
-
 $clubs = selectClubs();
 include "view-clubs.php";
 include "view-footer.php";
