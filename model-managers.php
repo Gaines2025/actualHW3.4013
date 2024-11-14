a46a330e90076d95a87e5488eb0f4326f1705b1c
Gaines2025 <maalakai.gaines@ou.edu>
2024-11-13 19:04:48 -0600

    Update model-managers.php

--- model-managers.php
+++ model-managers.php
@@ -30,7 +30,7 @@ function updateManagers($mName, $mCountry, $mClub, $mAge, $mid) {
 function insertManagers($mName, $mCountry, $mClub, $mAge) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO Manager ('manager_ID', 'manager_name', 'manager_country', 'manager_club', 'manager_age') VALUES (NULL, ?, ?, ?, ?)");
+        $stmt = $conn->prepare("INSERT INTO `Manager` (`manager_ID`, `manager_name`, `manager_country`, `manager_club`, `manager_age`) VALUES (NULL, ?, ?, ?, ?)");
         $stmt->bind_param("sssi", $cName, $cCity, $cCountry, $mAge);
         $success = $stmt->execute();
         $conn->close();
