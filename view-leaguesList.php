ac36d0040af06bd99bcfc4a0768dc83c2cfe0979
Gaines2025 <maalakai.gaines@ou.edu>
2024-12-05 19:00:01 -0600

    Update view-leaguesList.php

--- view-leaguesList.php
+++ view-leaguesList.php
@@ -1,6 +1,6 @@
 <select class="form-select" id="lid" name="lid">
 <?php
-while ($leagueNames = selectLeaguesFromInput->fetch_assoc()) {
+while ($leagueStuff = $leagueNames->fetch_assoc()) {
 ?>
     <option value="<?php echo $leagueNames['league_id']; ?>"><?php echo $leagueNames['league_name']; ?></option>
 <?php
