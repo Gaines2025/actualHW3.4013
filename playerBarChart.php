76911d342122f4383ada227d46da915b469f2977
Gaines2025 <maalakai.gaines@ou.edu>
2024-12-09 14:48:45 -0600

    Update playerBarChart.php

--- playerBarChart.php
+++ playerBarChart.php
@@ -30,3 +30,25 @@
     }
   });
 </script>
+<script>
+  const ctx = document.getElementById('myChart1');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
