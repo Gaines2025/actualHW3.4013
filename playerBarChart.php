9d8fd6f4762269bd7d803ae346839e786ebfe2fc
Gaines2025 <maalakai.gaines@ou.edu>
2024-12-09 15:13:24 -0600

    Update playerBarChart.php

--- playerBarChart.php
+++ playerBarChart.php
@@ -13,7 +13,7 @@
     data: {
       labels: ['Erling Haaland', 'Cole Palmer', 'Lionel Messi', 'Lamine Yamal', 'Khvicha Kvaratskhelia', 'Florian Wirts', 'Bradley Barcola', 'Darwin Nunez', 'Luis Suarez'],
       datasets: [{
-        label: '# of Votes',
+        label: 'Player Age',
         data: [24, 22, 37, 18, 23, 21, 22, 23, 36],
         borderWidth: 1
       }]
