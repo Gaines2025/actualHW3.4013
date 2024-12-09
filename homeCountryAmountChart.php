a619849030e3d2217f4de286a0270ded8710f1f3
Gaines2025 <maalakai.gaines@ou.edu>
2024-12-09 15:04:56 -0600

    Create homeCountryAmountChart.php

---
+++ homeCountryAmountChart.php
@@ -0,0 +1,11 @@
+<?php
+require_once("util-db.php");
+require_once("model-players.php");
+
+$pageTitle = "Futbol Players";
+include "view-header.php";
+
+$players = selectPlayers();
+include "countryAmountChart.php";
+include "view-footer.php";
+?>
